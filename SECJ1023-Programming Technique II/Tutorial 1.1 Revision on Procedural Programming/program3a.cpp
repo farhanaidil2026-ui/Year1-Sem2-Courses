2ae8dfebd7d87952bd13981af2f00263162d4add
Aidil Farhan <farhanaidil2026@gmail.com>
2026-03-17 07:58:44 +0800

    Add files via upload

---
+++ SECJ1023-Programming Technique II/Tutorial 1.1 Revision on Procedural Programming/program3a.cpp
@@ -0,0 +1,28 @@
+// Programming Technique II (SECJ1023)
+// Revision and Level Test of Procedural Programming
+// Program 3: Find the sum of a list of numbers
+// Name: XXXXXX (Matrics No.)
+// Date: XXXXXX
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m, n;
+
+    cout << "Enter the starting number  => ";
+    cin >> m;
+
+    cout << "Enter the ending number  => ";
+    cin >> n;
+
+    cout << endl;
+    cout << "The sum is " << endl;
+
+    system("pause");
+
+    return 0;
+}
+
---
+++ SECJ1023-Programming Technique II/Tutorial 1.1 Revision on Procedural Programming/program3b.cpp
@@ -0,0 +1,42 @@
+// Programming Technique II (SECJ1023)
+// ? Revision and Level Test of Procedural Programming
+// ! Program 3b: Calculate statistics (largest, sum and mean) of data
+
+//? Collaborative Coding Group Members:
+// Name 1:
+// Name 2:
+// Name 3:
+// Name 4:
+
+#include <iostream>
+
+using namespace std;
+
+//! Define three functions: largest, sum, mean
+
+
+int main()
+{
+    int n;
+    
+
+    cout << "How many numbers do you want to enter?  => ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter number #" << i+1 << "  => ";
+
+    }
+
+    cout << endl;
+    
+    cout << "Largest :  " << endl;
+    cout << "Sum     :  " << endl;
+    cout << "Mean    :  " << endl;
+    cout << endl;
+
+    system("pause");
+
+    return 0;
+}
