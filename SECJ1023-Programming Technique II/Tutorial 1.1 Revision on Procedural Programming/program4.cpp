ce9b2c6f27307d9e335a648d1f469f686ee6da82
Aidil Farhan <farhanaidil2026@gmail.com>
2026-03-17 07:59:07 +0800

    Add files via upload

---
+++ SECJ1023-Programming Technique II/Tutorial 1.1 Revision on Procedural Programming/program4.cpp
@@ -0,0 +1,26 @@
+// Programming Technique II (SECJ1023)
+// Revision and Level Test of Procedural Programming
+// Program 4: Date conversion 1
+// Name: XXXXXX (Matrics No.)
+// Date: XXXXXX
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    int d, m, y;  // Day, Month, Year
+
+    cout << "Enter your DOB (dd mm yy) => ";
+    cin >> d >> m >> y;
+
+    cout << endl;
+    cout << "Your Date of Birth is " << endl;
+
+    system("pause");
+
+    return 0;
+}
+
---
+++ SECJ1023-Programming Technique II/Tutorial 1.1 Revision on Procedural Programming/program5.cpp
@@ -0,0 +1,27 @@
+// Programming Technique II (SECJ1023)
+// Revision and Level Test of Procedural Programming
+// Program 5: Date conversion 2
+// Name: XXXXXX (Matrics No.)
+// Date: XXXXXX
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char month[4];
+    int day;
+    int year;
+
+    cout << "Enter your DOB (Mmm dd yyyy) => ";
+    cin >> month >> day >> year;
+
+    cout << endl;
+    cout << "Your Date of Birth is " << endl;
+
+    system("pause");
+
+    return 0;
+}
