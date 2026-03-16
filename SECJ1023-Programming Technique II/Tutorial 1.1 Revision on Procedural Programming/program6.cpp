3ddd95e21c0dd7c1106697bb4796847ea4a67b68
Aidil Farhan <farhanaidil2026@gmail.com>
2026-03-17 07:59:27 +0800

    Add files via upload

---
+++ SECJ1023-Programming Technique II/Tutorial 1.1 Revision on Procedural Programming/program6.cpp
@@ -0,0 +1,63 @@
+// Programming Technique II (SECJ1023)
+// Revision and Level Test of Procedural Programming
+// Program 6: Find the covariance between two lists of numbers
+// Name: XXXXXX (Matrics No.)
+// Date: XXXXXX
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// ! readArrayFromFile:
+// ? This function reads the list of numbers from a text file specified by the "fileName"
+// ? and stores the read numbers into the parameter list.
+// ? It also returns the size of the list (i.e, the how many numbers read)
+// ?
+// ! Example usage:
+// int size;
+// double array[20];
+// size = readArrayFromFile("myNumberFile.txt", array);
+
+int readArrayFromFile(const char *fileName, double list[])
+{
+    ifstream fin;
+    int count = 0; //The size of the list
+    int number;    //The current number read from the file
+
+    fin.open(fileName);
+
+    while (fin >> number)
+    {
+        list[count] = number;
+        count++;
+    }
+    fin.close();
+
+    return count;
+
+    fin.open(fileName);
+}
+
+int main()
+{
+    char fileName1[20];
+    char fileName2[20];
+
+    double list1[10];  // ? To hold the list of numbers read from the first file
+    double list2[10];  // ? To hold the list of numbers read from the second file
+    int size;          // ? How many numbers read
+
+    cout << "Enter file #1 => ";
+    cin >> fileName1;
+
+    cout << "Enter file #2 => ";
+    cin >> fileName2;
+
+    cout << endl;
+    cout << "Covariance = " << endl;
+
+    system("pause");
+
+    return 0;
+}
