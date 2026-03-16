2be9a9204fdae242f856ca433aa64f4a953921c6
Aidil Farhan <farhanaidil2026@gmail.com>
2026-03-17 07:58:21 +0800

    Add files via upload

---
+++ SECJ1023-Programming Technique II/Tutorial 1.1 Revision on Procedural Programming/program1.cpp
@@ -0,0 +1,17 @@
+// Programming Technique II (SECJ1023)
+// Revision and Level Test of Procedural Programming
+// Program 1: Simple calculator
+// Name: XXXXXX (Matrics No.)
+// Date: XXXXXX
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    system("pause");
+
+    return 0;
+}
---
+++ SECJ1023-Programming Technique II/Tutorial 1.1 Revision on Procedural Programming/program2.cpp
@@ -0,0 +1,26 @@
+// Programming Technique II (SECJ1023)
+// Revision and Level Test of Procedural Programming
+// Program 2: Find the largest number
+// Name: XXXXXX (Matrics No.)
+// Date: XXXXXX
+
+#include <iostream>
+
+using namespace std;
+
+//Define a function that determines the largest number from two numbers
+
+int main()
+{
+    int a, b, c, d;
+
+    cout << "Please enter the four numbers => ";
+    cin >> a >> b >> c >> d;
+
+    cout << endl;
+    cout << "The largest value is " << endl;
+
+    system("pause");
+
+    return 0;
+}
