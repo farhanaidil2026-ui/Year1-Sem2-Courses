7c0f09741c7808f40002a4e833155f90341b42d7
Aidil Farhan <farhanaidil2026@gmail.com>
2026-03-18 09:12:13 +0800

    Add files via upload

---
+++ SECJ1023-Programming Technique II/Tutorial 1.2: Procedural Programming (PP) vs Object-Oriented Programming (OOP)/program2.cpp
@@ -0,0 +1,41 @@
+// Programming Technique II (SCSJ1023)
+// Tutorial 1: Procedural Programming (PP) vs. Object-Oriented Programming (OOP)
+// Program 2: Using PP with struct
+// Use the following coordinates: (1, 2) and (3, 4) for Circle 1 and 2 to test your code
+// The distance and slope between these two circle centres are 2.83 and 1 respectively
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// ! Task 1: Define a struct data type to hold information about a circle
+
+
+
+// ! Task 2: Define a function to calculate the distance between two circle centres.
+
+
+
+// ! Task 3: Define a function to calculate the slope between two circle centres.
+
+
+
+
+int main()
+{
+	// ! Task 4: Define a circle with specified information from the struct data type
+
+
+
+
+	// ! Task 5: Calculate the distance and slope between two circle centres
+	
+
+
+
+	// ! Task 6: Display the distance and slope between two circle centres
+	
+
+
+	return 0;
+}
