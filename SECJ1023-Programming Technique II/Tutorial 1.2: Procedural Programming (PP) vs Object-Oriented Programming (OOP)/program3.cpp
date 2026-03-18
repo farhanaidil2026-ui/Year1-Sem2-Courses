a659be589f87784357d989458b677a99b89ddb10
Aidil Farhan <farhanaidil2026@gmail.com>
2026-03-18 09:12:29 +0800

    Add files via upload

---
+++ SECJ1023-Programming Technique II/Tutorial 1.2: Procedural Programming (PP) vs Object-Oriented Programming (OOP)/program3.cpp
@@ -0,0 +1,32 @@
+// Programming Technique II (SCSJ1023)
+// Tutorial 1: Procedural Programming (PP) vs. Object-Oriented Programming (OOP)
+// Program 3: Using OOP
+// Use the following coordinates: (1, 2) and (3, 4) for Circle 1 and 2 to test your code
+// The distance and slope between these two circle centres are 2.83 and 1 respectively
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// ! Task 1: Define a class to represent the structure of a circle.
+// !         It consists of information the circle's centre coordinate. 
+// !         The operations that the circle should have.
+
+
+
+int main()
+{
+	// ! Task 2: Create a circle object with specified information
+
+
+
+	// ! Task 3: Calculate the distance and slope between two circle centres
+	
+
+
+	// ! Task 4: Display the distance and slope between two circle centres
+	
+
+
+	return 0;
+}
